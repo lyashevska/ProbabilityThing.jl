da4683cd8c17bda2acf9841f7e92f79ae9348741
Documenter.jl <documenter@juliadocs.github.io>
2023-06-12 13:51:22 +0000

    build based on 7664a78

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ProbabilityThing","category":"page"},{"location":"#ProbabilityThing","page":"Home","title":"ProbabilityThing","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ProbabilityThing.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ProbabilityThing]","category":"page"}]
+}
